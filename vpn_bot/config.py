85f0c3c6a3537fe937fd39e71d10ac4333bab061
Alchemmist <anton.ingrish@gmail.com>
2024-08-21 10:32:41 +0300

    refacor

--- vpn_bot/config.py
+++ vpn_bot/config.py
@@ -4,10 +4,11 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+
+DATE_FORMAT = "%m.%Y"
 
 # Callback patterns
 MONTHLY_ANALYTICS_CALLBACK_PATTERN = "monthly_analytics_"
+PAYMENT_CALLBACK_PATTERN = "monthly_analytics_"
 
-DATE_FORMAT = "%m.%Y"
